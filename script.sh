87b8dcb6fee5f118c0a4f0a72f894fcb28554534
SanthoshCoding <santhosh.nagendra64@gmail.com>
2021-04-20 15:58:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
- docker run --name my-nginx2333 -p 8081:80 -v /home/santhosh/frontend/:/usr/share/nginx/html -d nginx
+ docker run --name my-nginx2333 -p 8082:80 -v /home/santhosh/frontend/:/usr/share/nginx/html -d nginx
